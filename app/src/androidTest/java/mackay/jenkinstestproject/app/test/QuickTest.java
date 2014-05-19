64ad553e773b198f48a5b9cebcf7de3cb209d6c1
Byron Mackay <byronm@mediarain.com>
2014-05-19 08:32:08 -0600

    more stuff

--- app/src/androidTest/java/mackay/jenkinstestproject/app/test/QuickTest.java
+++ app/src/androidTest/java/mackay/jenkinstestproject/app/test/QuickTest.java
@@ -1,7 +1,27 @@
 package mackay.jenkinstestproject.app.test;
 
+import android.os.SystemClock;
+import android.test.ActivityInstrumentationTestCase2;
+
+import mackay.jenkinstestproject.app.MainActivity;
+
 /**
  * Created by bqmackay on 5/17/14.
  */
-public class QuickTest {
+public class QuickTest extends ActivityInstrumentationTestCase2<MainActivity>{
+
+    public QuickTest() {
+        super("mackay.jenkinstestproject.app.test", MainActivity.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        getActivity();
+    }
+
+    public void testName() throws Exception {
+        SystemClock.sleep(2000);
+        assertTrue(true);
+    }
 }
